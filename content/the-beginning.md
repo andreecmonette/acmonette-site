fd3b81ba5eb282696969e0fdf3afa328b6a7154c
Andree Monette <andreecmonette@gmail.com>
2014-03-10 15:00:07 -0400

    Initial commit

---
+++ content/the-beginning.md
@@ -0,0 +1,11 @@
+Title: Creating an online presence
+Date: 2014-01-24 01:00
+Category: Blog
+Tags: beginning, musing
+Slug: the-beginning
+Author: Andree Monette
+Summary: Starting out
+
+Embarking on a new journey is always interesting. I’ve lived on the internet as long as I can remember, but I haven’t really made a concentrated effort before now to make an online presence under my “day identity”. So this is my first real foray into that whole world. I get the sense it’s different from expressing oneself pseudonymously, as in that case there’s a sort of “identity management” that can go on – you can be anyone to anything at a click. So a major challenge here is going to be blogging relatively uninhibited given that the stakes are a bit higher with one’s name.
+
+Besides that, I’m expecting in a while to look back on these early blog posts as a sign of how I’ve improved. So I’m not that worried if they’re not as polished as they eventually become. Generally that pattern holds in life, and I expect it to hold in blogging too.
---
+++ pelicanconf.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Andree Monette'
+SITENAME = u'Andree Monette'
+SITEURL = 'andreecmonette.github.io'
+
+SITESUBTITLE = 'Thoughts, in stereo.'
+SITETAG = "and cat mon"
+
+STYLESHEETS = ("pygment.css", "voidybootstrap.css",)
+
+CUSTOM_ARTICLE_SHARING = "sharing.html"
+CUSTOM_ARTICLE_SCRIPTS = "sharing_scripts.html"
+
+TIMEZONE = 'America/Toronto'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_DOMAIN = SITEURL
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+TRANSLATION_FEED_ATOM = None
+TAG_FEED_ATOM = 'feeds/tags/%s.atom.xml'
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'http://twitter.com/andreecmonette','fa fa-twitter-square fa-fw fa-lg'),
+    ('GitHub', 'http://github.com/andreecmonette','fa fa-github-square fa-fw fa-lg'),)
+
+DEFAULT_PAGINATION = 10
+
+THEME = "pelican-themes/voidy-bootstrap"
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
